7f6b54e27b833e853016a4ef3b3d864a336f51fc
marlinaspanel <marlina.spanel@gmail.com>
2023-11-28 13:05:59 +0100

    added Neoletter forms

--- src/Widgets/index.ts
+++ src/Widgets/index.ts
@@ -2,4 +2,7 @@ import.meta.glob(['./**/*WidgetClass.ts', './**/*WidgetComponent.tsx'], {
   eager: true,
 })
 
+import { initNeoletterFormWidgets } from 'scrivito-neoletter-form-widgets'
+initNeoletterFormWidgets("bf49f5426b2b1ee70ac419970b3b3291");
+
 export {}
